89d3f3f767a8eda6dceab9bd89e14b15eb08157e
Abubakar <abubakaran102025@gmail.com>
2024-04-13 18:28:49 +0500

    Learned about the layouts, route groups , priavte folder, file colocation

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Details About the Review Number {params.reviewId} for {params.productId}!
